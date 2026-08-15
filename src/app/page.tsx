cd753ecbc33fa299f760d167c40165922a0daac6
wiktoria <wika18wika@gmail.com>
2026-08-16 00:13:52 +0200

    clean next.js template

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Travel Map App</h1>
+      <p className="mt-4">Nasz projekt ruszył!</p>
+    </main>
+  );
+}
